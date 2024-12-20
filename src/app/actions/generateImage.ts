2308c1fc793ccdb443ab0f508983679153405012
Seth DeWhitt <seth_dewhitt@outlook.com>
2024-12-20 04:02:29 -0800

    refactoring for security

---
+++ src/app/actions/generateImage.ts
@@ -0,0 +1,23 @@
+"use server";
+
+export async function generateImage(text: string) {
+    try {
+        const response = await fetch(`https://localhost:3000/api/generate-image`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text }),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error("Error:", error);
+        return {success: false, error: error instanceof Error ? error.message : "Failed to generate image"};
+    }
+}
