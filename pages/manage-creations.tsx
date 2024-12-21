3dbc68d9b181a3353f3ae8ec35e41289e36a16e2
Seth DeWhitt <seth_dewhitt@outlook.com>
2024-12-21 01:29:47 -0800

    version control nightmare avoided

---
+++ pages/manage-creations.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageCreations = () => {
+  return (
+    <div>
+      <h1>Your Image Library</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default ManageCreations;
