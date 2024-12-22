cdcd6e763e18ce124ce470bf333cd8ec9af758fd
Seth DeWhitt <seth_dewhitt@outlook.com>
2024-12-21 19:34:34 -0800

    Minor tweaks

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Sidebar.css'; // Import the CSS file
 
 interface SidebarProps {
   images: { url: string }[];
@@ -9,7 +10,7 @@ const Sidebar: React.FC<SidebarProps> = ({ images, onImageClick }) => {
   return (
     <div className="w-64 bg-indigo-900 text-white p-4 flex flex-col h-screen">
       <h2 className="text-l font-semibold mb-4 center">Image Library</h2>
-      <div className="overflow-y-auto flex-1">
+      <div className="overflow-y-auto flex-1 custom-scrollbar">
         {images.map((image, index) => (
           <div key={index} className="mb-4">
             <img
